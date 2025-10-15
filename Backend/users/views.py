e14a5541d6c31ba1bc426c29910ac426446b44c8
bohdan_m <sqwfunny@gmail.com>
2025-10-15 17:15:47 +0200

    Created registration and authentication using DRF JWT

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserRegister(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
